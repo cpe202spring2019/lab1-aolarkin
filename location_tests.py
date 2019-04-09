53f5de5a3707d5b10088ad94d49e36045a5a0039
aolarkin <aolarkin@calpoly.edu>
2019-04-08 18:26:26 -0700

    Location tests submission

--- location_tests.py
+++ location_tests.py
@@ -6,8 +6,19 @@ class TestLab1(unittest.TestCase):
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
-    # Add more tests!
+
+        loc = Location('Paris', 48.9, 2.4)
+        self.assertEqual(repr(loc),"Location('Paris', 48.9, 2.4)")
+
+    def test_eq(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location('Paris', 48.9, 2.4)
+        self.assertFalse(loc1 == loc2)
+
+        loc3 = Location("SLO", 35.3, -120.7)
+        loc4 = Location("SLO", 35.3, -120.7)
+        self.assertTrue(loc3 == loc4)
+        
 
 if __name__ == "__main__":
         unittest.main()
